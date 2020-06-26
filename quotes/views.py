f78e28a57c5912943542a52c72782e9185406c76
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-06-26 15:31:02 -0700

    add if condition for ticker request
    add ticker request method to search a particular stocks

--- quotes/views.py
+++ quotes/views.py
@@ -4,15 +4,24 @@ def home(request):
     import requests
     import json
 
-    # pk_08c7a6a3676c470d899103c8e2ed5602
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_08c7a6a3676c470d899103c8e2ed5602")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        # pk_08c7a6a3676c470d899103c8e2ed5602
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_08c7a6a3676c470d899103c8e2ed5602")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
 
-    return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol above..."})
+
+
+
+
+
 
 def about(request):
     return render(request, 'about.html', {})
