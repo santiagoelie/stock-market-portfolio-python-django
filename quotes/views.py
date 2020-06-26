23a3edffef0a3cad999ad6d5e5e032b701ff9f9f
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-06-26 11:01:12 -0700

    add API requests
    add API requests and pull api stock

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    # pk_08c7a6a3676c470d899103c8e2ed5602
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aap1/quote?token=pk_08c7a6a3676c470d899103c8e2ed5602")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
