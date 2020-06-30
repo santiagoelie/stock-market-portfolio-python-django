ecc6cc7f853a791236070f8da7989120d45154fb
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-06-29 21:26:33 -0700

    import items and add if else statement
    import forms, stockform and messages. And add if else statement to get form for StockForm.

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 def home(request):
     import requests
@@ -26,6 +28,14 @@ def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
 
-    ticker = Stock.objects.all()
-    return render(request, 'add_stock.html', {'ticker': ticker})
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock has been added!"))
+            return redirect('add_stock')
+
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
