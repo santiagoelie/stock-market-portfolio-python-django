ca751d97f3f8757c4b8517db57081a494d5c3900
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-07-10 10:38:01 -0700

    add delete_stock item and update add_stock
    update add_stock for api request and add delete_stock def

--- quotes/views.py
+++ quotes/views.py
@@ -29,6 +29,9 @@ def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
+    import requests
+    import json
+
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -39,11 +42,25 @@ def add_stock(request):
 
     else:
         ticker = Stock.objects.all()
-        return render(request, 'add_stock.html', {'ticker': ticker})
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_08c7a6a3676c470d899103c8e2ed5602")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
 
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ("Stock has been deleted!"))
-    return redirect(add_stock)
+    return redirect(delete_stock)
+
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, 'delete_stock.html', {'ticker': ticker})
